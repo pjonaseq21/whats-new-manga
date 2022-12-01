ad75f54918a5b2e947f4f139b3e6e09e9202f005
Your Name <you@example.com>
2022-12-01 09:19:40 +0100

    test

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const session = require('express-session');
 const request = require('request');
 const jsdom = require("jsdom");
-const { json } = require("body-parser");
 const { JSDOM } = jsdom;
 
 app.use(session({
@@ -16,15 +15,29 @@ app.set("view engine", 'ejs')
 app.use(express.static(__dirname+'/public'));
 
 app.get("/",(req,res)=>{
-    request("https://readberserk.com/",function test(error,response){
-        let data = new JSDOM(response.body)
+    
+   
+request("https://readberserk.com/",function test(error,response){
 
+        let data = new JSDOM(response.body)
         let title = data.window.document.querySelector('#content > div > div.col-md-8 > div.row.mb-3 > div:nth-child(3) > div > div > h5').innerHTML
         let href = data.window.document.querySelector('#content > div > div.col-md-8 > div.row.mb-3 > div:nth-child(3) > div > div > a').href
         
-        res.render("index.ejs",{title:title,href:href})
+    }
+    
+
+    )
+const chainsaw = request("https://www.chainsaw-man-manga.online/",function test2(error,response){
+        let data = new JSDOM(response.body)
+        let title2 = data.window.document.querySelector('#ceo_latest_comics_widget-3 > ul > li:nth-child(1) > a').innerHTML
+        const test = [title2]
+        return test
 
     })
+    
+    console.log(chainsaw)
+
+    res.render("index.ejs")
 
 })
 

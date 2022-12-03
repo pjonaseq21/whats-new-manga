7587c552d76a0148e4bf87ad46665b3da8c2ed55
Your Name <pjonas@example.com>
2022-12-03 10:50:26 +0100

    promises added

--- server.js
+++ server.js
@@ -16,28 +16,40 @@ app.use(express.static(__dirname+'/public'));
 
 app.get("/",(req,res)=>{
     
-   
-request("https://readberserk.com/",function test(error,response){
+async function GetchainsawMan(){ 
+        return new Promise((resolve,reject)=>{
+          request("https://www.chainsaw-man-manga.online/",function test2(error,response){
+            let data = new JSDOM(response.body);
+            let title2 = data.window.document.querySelector('#ceo_latest_comics_widget-3 > ul > li:nth-child(1) > a').innerHTML;
+            let href = data.window.document.querySelector("#menu-item-1867 > a").href;
+            resolve({title2,href});
+        })
+        })
+             
+  
+  }
+async function getData() {
+        return new Promise((resolve, reject) => {
+          request("https://readberserk.com/", (error, response) => {
+            let data = new JSDOM(response.body);
+            let title = data.window.document.querySelector(
+              "#content > div > div.col-md-8 > div.row.mb-3 > div:nth-child(3) > div > div > h5"
+            ).innerHTML;
+            let href = data.window.document.querySelector(
+              "#content > div > div.col-md-8 > div.row.mb-3 > div:nth-child(3) > div > div > a"
+            ).href;
+            resolve({ title, href });
+          });
+        });
+      }
+      
+      (async() => {
+        let databerserk = await getData();
+        let datachain = await GetchainsawMan();
+        console.log(databerserk, datachain);
+        res.render("index.ejs",{data:databerserk,dataChainsawMan: datachain})
+      })();
 
-        let data = new JSDOM(response.body)
-        let title = data.window.document.querySelector('#content > div > div.col-md-8 > div.row.mb-3 > div:nth-child(3) > div > div > h5').innerHTML
-        let href = data.window.document.querySelector('#content > div > div.col-md-8 > div.row.mb-3 > div:nth-child(3) > div > div > a').href
-        
-    }
-    
-
-    )
-const chainsaw = request("https://www.chainsaw-man-manga.online/",function test2(error,response){
-        let data = new JSDOM(response.body)
-        let title2 = data.window.document.querySelector('#ceo_latest_comics_widget-3 > ul > li:nth-child(1) > a').innerHTML
-        const test = [title2]
-        return test
-
-    })
-    
-    console.log(chainsaw)
-
-    res.render("index.ejs")
 
 })
 
